4fd8cd0f911a5d0e9fdbe24ff58e38c2d3fd2188
Inheert <theoclaereboudt@gmail.com>
2024-07-10 04:41:50 +0200

    test

--- src/main.c
+++ src/main.c
@@ -3,12 +3,14 @@
 int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
+	char	test[1000];
 
 	while(1)
 	{
 		input = readline("Enter text: ");
 		add_history(input);
-		printf("%s\n", input);
+		getcwd(test, 1000);
+		printf("%s %s\n", getcwd(test, 1000), input);
 	}
 	argc = (int)argc;
 	argv = (char **)argv;
