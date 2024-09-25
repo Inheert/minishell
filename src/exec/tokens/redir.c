6e1b28445dffdd6c18ff0372aee78c1c8a591746
Inheert <theoclaereboudt@gmail.com>
2024-09-25 17:34:55 +0200

    segfault resolve when the first arg is a redir

--- src/exec/tokens/redir.c
+++ src/exec/tokens/redir.c
@@ -6,7 +6,7 @@
 /*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 16:59:46 by tclaereb          #+#    #+#             */
-/*   Updated: 2024/09/10 17:06:50 by tclaereb         ###   ########.fr       */
+/*   Updated: 2024/09/25 17:32:35 by tclaereb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ t_token	*ft_redir_in(t_pipe *pipes, t_token *token, int *fdin)
 
 t_token	*ft_redir_out(t_pipe *pipes, t_token *token, int *fdout)
 {
-	fprintf(stderr, "%d\n", token->token == REDIR_APPEND_OUT);
 	if (token->token == REDIR_OUT)
 		*fdout = open(token->str, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	else
--- src/tokenisation/parse_tokens.c
+++ src/tokenisation/parse_tokens.c
@@ -3,40 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   parse_tokens.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cluby <cluby@student.42.fr>                +#+  +:+       +#+        */
+/*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 18:59:31 by cluby             #+#    #+#             */
-/*   Updated: 2024/09/19 04:23:23 by cluby            ###   ########.fr       */
+/*   Updated: 2024/09/25 17:31:40 by tclaereb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	redir(t_token **token)
+static int	redir(t_token *token, t_token **token_origin)
 {
 	t_token *tmp;
 
-	if (!(*token))
+	if (!token)
 		return (0);
-	tmp = *token;
-	if ((*token)->str[0] == '<' || (*token)->str[0] == '>')
+	tmp = token;
+	if (token->str[0] == '<' || token->str[0] == '>')
 	{
 		if (!tmp->next)
 			return (0); //bash: syntax error near unexpected token `token->str' (ou 'newline')
 		if (tmp->next->token == BLANK && !tmp->next->next)
 			return (0); //bash: syntax error near unexpected token `token->str' (ou 'newline')
 		if (tmp->next->token == BLANK)
-			tmp = tmp->next->next; 
+			tmp = tmp->next->next;
 		else
 			tmp = tmp->next;
 		if (tmp->token == PIPE || tmp->token == REDIR_APPEND_OUT || tmp->token == HERE_DOC || tmp->token == REDIR_IN || tmp->token == REDIR_OUT)
 		{
 			return (0); //bash: syntax error near unexpected token `token->str'
 		}
-		tmp->token = (*token)->token;
-		tmp = *token;
-		*token = (*token)->next;
-		t_token_del(token, tmp);
+		tmp->token = token->token;
+		// printf("TOKEN %d %s, next: %p\n", tmp->token, tmp->str, tmp->next);
+		t_token_del(token_origin, token);
 	}
 	return (1);
 }
@@ -64,16 +63,20 @@ static void	env(t_token **token, t_envp *menvp)
 	}
 }
 
-int	parse_tokens(t_token *token, t_envp *menvp)
+int	parse_tokens(t_token **token, t_envp *menvp)
 {
-	while (token)
+	t_token	*tmp;
+
+	tmp = *token;
+	while (tmp)
 	{
-		env(&token, menvp);
-		if (!token)
+		env(token, menvp);
+		if (!*token)
 			return (0);
-		if (!redir(&token))
+		if (!redir(tmp, token))
 			return (0);
-		token = token->next;
+		// printf("%d %s actual token adress: %p\n", token->token, token->str, token);
+		tmp = tmp->next;
 	}
 	return (1);
 }
